74241e7e3f131a8002092edc7c2fb2fbb32220bd
HowardHughes98 <82488909+HowardHughes98@users.noreply.github.com>
2021-04-28 10:06:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+var time = +prompt('введите время');
+
+
+switch (time) {
+    case 01:
+       alert('час ночи');
+       break;
+    case 02:
+        alert('два часа ночи');
+        break;
+    case 03:
+        alert ('три часа ночи');
+        break;
+    case 04:
+        alert('четыре часа ночи');
+        break;
+    case 05:
+        alert('пять часов утра');
+        break;
+    case 06:
+        alert('шесть часов утра');
+        break;
+    case 07:
+        alert('семь часов утра');
+        break;
+    case 08:
+        alert('восемь часов утра');
+        break;
+    case 09:
+        alert('девять часов утра');
+        break;
+    case 10:
+        alert('десять часов утра');
+        break;
+    case 11:
+        alert('одиннадцать часов утра');
+        break;
+    case 12:
+        alert('пол день');
+        break;
+    case 13:
+        alert('час дня');
+        break;
+    case 14:
+        alert('два часа дня');
+    case 15:
+        alert('три часа дня');
+        break;
+    case 16:
+        alert('4 часа вечера');
+        break;
+    case 17:
+        alert('пять часов вечера');
+        break;
+    case 18:
+        alert('шесть часов вечера');
+        break;
+    case 19:
+        alert('семь часов вечера');
+        break;
+    case 20:
+        alert('восемь часов вечера');
+        break;
+    case 21:
+        alert('девять часов ночи');
+        break;
+    case 22:
+        alert('десять часов ночи');
+        break;
+    case 23:
+        alert('одиннадцать часов ночи');
+        break;
+    case 24:
+        alert('двеннадцать часов ночи');
+        break;
+            
+}
